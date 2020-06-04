f39f1c16cd68f12d092678531ec987278458fa9d
Lakshmeesh-H <lakshmeesh.h@gmail.com>
2020-06-04 22:06:07 +0530

    Getting the results from api

--- mainpage.py
+++ mainpage.py
@@ -4,18 +4,17 @@ import json
 r = requests.get('https://formulae.brew.sh/api/formula.json')
 packages_json = r.json()
 
-package_name = packages_json[0]['name']
-package_desc = packages_json[0]['desc']
+for package in packages_json:
+    package_name = package['name']
+    package_desc = package['desc']
 
-package_url = f'https://formulae.brew.sh/api/formula/{package_name}.json'
+    package_url = f'https://formulae.brew.sh/api/formula/{package_name}.json'
 
-r = requests.get(package_url)
-package_json = r.json()
+    r = requests.get(package_url)
+    package_json = r.json()
 
-package_str = json.dumps(package_json, indent=2)
+    installs_30 = package_json['analytics']['install_on_request']['30d'][package_name]
+    installs_90 = package_json['analytics']['install_on_request']['90d'][package_name]
+    installs_365 = package_json['analytics']['install_on_request']['365d'][package_name]
 
-installs_30 = package_json['analytics']['install_on_request']['30d'][package_name]
-installs_90 = package_json['analytics']['install_on_request']['90d'][package_name]
-installs_365 = package_json['analytics']['install_on_request']['365d'][package_name]
-
-print(package_name, package_desc, installs_30, installs_90, installs_365)
+    print(package_name, package_desc, installs_30, installs_90, installs_365)
